ec34ebf6565086e023eb66a5c50a6a5eb487785e
hhh <1340545944@qq.com>
2021-06-18 19:53:23 +0800

    reset

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './common';
-export * from './rules/rule';
-export * from './rules/rules';
-export * from './libs/libs';
-export * from './builtins/builtins';
-export * from './eval';
--- src/libs/libs.ts
+++
@@ -1,8 +0,0 @@
-import { Dict } from '../common';
-import { math } from './math';
-import { func_tools } from './func_tools';
-
-export const libs = new Map<string, Dict>([
-    ['math', math],
-    ['func_tools', func_tools],
-]);
