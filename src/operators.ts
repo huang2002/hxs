86d8c70db31eda2a34e192bd0acc624799c7c58e
hhh <1340545944@qq.com>
2021-06-29 15:44:34 +0800

    improve code style & add callback invocation

--- src/operators.ts
+++ src/operators.ts
@@ -1,6 +1,6 @@
 import { SpanNode, SymbolNode, WordNode } from '3h-ast';
 import { Dict, FunctionHandler, ContextValue, SyntaxHandler, Utils } from './common';
-import { evalBufferNode, evalExpression, evalList, evalNode } from './eval';
+import { evalBufferNode, evalExpression, evalList, evalNode, evalNodes } from './eval';
 import { createInlineFunction } from './function';
 
 export interface OperatorDefinition {
@@ -45,14 +45,17 @@ export const operators: readonly OperatorDefinition[] = [{
     symbol: '.',
     priority: 1,
     handler(buffer, index, context) {
+
         const target = evalBufferNode(buffer, index - 1, buffer[index], context);
         if (!Utils.isDict(target)) {
             Utils.raise(TypeError, 'expect a dict', buffer[index - 1], context);
         }
+
         const nameNode = buffer[index + 1];
         if (!nameNode || nameNode.type !== 'word') {
             Utils.raise(TypeError, 'expect a word following', buffer[index], context);
         }
+
         const name = (nameNode as WordNode).value;
         let value: ContextValue;
         if (name in (target as Dict)) {
@@ -60,65 +63,88 @@ export const operators: readonly OperatorDefinition[] = [{
         } else {
             value = null;
         }
+
         const valueNode = Utils.createValueNode(value, buffer[index]);
         Utils.replaceBuffer(buffer, index - 1, 3, valueNode);
+
     },
 }, {
     symbol: '[',
     priority: 1,
     handler(buffer, index, context) {
+
         if (index === 0 || buffer[index - 1].type === 'symbol') { // array creation
+
             const value = evalList((buffer[index] as SpanNode).body, context);
             buffer[index] = Utils.createValueNode(value, buffer[index]);
+
         } else { // index access
+
             const target = evalBufferNode(buffer, index - 1, buffer[index], context);
             if (!Utils.isDict(target)) {
                 Utils.raise(TypeError, 'expect a dict', buffer[index - 1], context);
             }
+
             const name = evalExpression((buffer[index] as SpanNode).body, context);
             if (typeof name !== 'string') {
                 Utils.raise(TypeError, 'expect a string inside', buffer[index], context);
             }
+
             let value: ContextValue;
             if ((name as string) in (target as Dict)) {
                 value = (target as Dict)[name as string];
             } else {
                 value = null;
             }
+
             const valueNode = Utils.createValueNode(value, buffer[index]);
             Utils.replaceBuffer(buffer, index - 1, 2, valueNode);
+
         }
+
     },
 }, {
     symbol: '(',
     priority: 1,
     handler(buffer, index, context) {
+
         if (index === 0 || buffer[index - 1].type === 'symbol') { // pure paratheses
+
             const value = evalExpression((buffer[index] as SpanNode).body, context);
             buffer[index] = Utils.createValueNode(value, buffer[index]);
+
         } else { // function call
+
             const handler = evalNode(buffer[index - 1], context);
             if (typeof handler !== 'function') {
                 Utils.raise(TypeError, 'expect a function', buffer[index], context);
             }
+
             const value = (handler as FunctionHandler)(
                 (buffer[index] as SpanNode).body,
                 buffer[index],
                 context,
             );
+
             const valueNode = Utils.createValueNode(value, buffer[index]);
             Utils.replaceBuffer(buffer, index - 1, 2, valueNode);
+
         }
+
     },
 }, {
     symbol: '{',
     priority: 1,
     handler(buffer, index, context) {
+
         if (index === 0 || buffer[index - 1].type === 'symbol') { // dict creation
+
             const result = Object.create(null) as Dict;
             const nodes = (buffer[index] as SpanNode).body;
+
             let left = 0;
             for (let right = 0; right < nodes.length; right++) {
+
                 const node = nodes[right];
                 if (node.type !== 'symbol' || node.value !== ',') {
                     if (right + 1 === nodes.length) {
@@ -127,6 +153,7 @@ export const operators: readonly OperatorDefinition[] = [{
                         continue;
                     }
                 }
+
                 let j;
                 for (j = left; j < right; j++) {
                     if (
@@ -136,20 +163,45 @@ export const operators: readonly OperatorDefinition[] = [{
                         break;
                     }
                 }
+
                 if (j === right) {
                     Utils.raise(SyntaxError, 'invalid dict creation', buffer[index], context);
                 }
+
                 const name = evalExpression(nodes, context, left, j);
-                const value = evalExpression(nodes, context, j + 1, right);
                 if (typeof name !== 'string') {
                     Utils.raise(TypeError, 'expect a string', buffer[left], context);
                 }
+
+                const value = evalExpression(nodes, context, j + 1, right);
                 result[name as string] = value;
+
                 left = right + 1;
+
             }
+
             buffer[index] = Utils.createValueNode(result, buffer[index]);
+
         } else { // callback invocation
-            // TODO:
+
+            const handler = evalNode(buffer[index - 1], context);
+            if (typeof handler !== 'function') {
+                Utils.raise(TypeError, 'expect a function', buffer[index], context);
+            }
+
+            const callback: FunctionHandler = (args, referer, _context) => {
+                return evalNodes((buffer[index] as SpanNode).body, _context);
+            };
+
+            const returnValue = (handler as FunctionHandler)(
+                [Utils.createValueNode(callback, buffer[index])],
+                buffer[index],
+                context,
+            );
+
+            const valueNode = Utils.createValueNode(returnValue, buffer[index]);
+            Utils.replaceBuffer(buffer, index - 1, 2, valueNode);
+
         }
 
     },
@@ -208,27 +260,36 @@ export const operators: readonly OperatorDefinition[] = [{
     symbol: '-',
     priority: 4,
     handler(buffer, index, context) {
+
         const b = evalBufferNode(buffer, index + 1, buffer[index], context);
         if (typeof b !== 'number') {
             Utils.raise(TypeError, 'expect a number', buffer[index - 1], context);
         }
+
         if (index === 0 || buffer[index - 1].type === 'symbol') { // unary
+
             const valueNode = Utils.createValueNode(
                 -(b as number),
                 buffer[index],
             );
             Utils.replaceBuffer(buffer, index, 2, valueNode);
+
         } else { // binary
+
             const a = evalBufferNode(buffer, index - 1, buffer[index], context);
             if (typeof a !== 'number') {
                 Utils.raise(TypeError, 'expect a number', buffer[index + 1], context);
             }
+
             const valueNode = Utils.createValueNode(
                 (a as number) - (b as number),
                 buffer[index],
             );
+
             Utils.replaceBuffer(buffer, index - 1, 3, valueNode);
+
         }
+
     },
 }, {
     symbol: '<',
@@ -324,17 +385,22 @@ export const operators: readonly OperatorDefinition[] = [{
     symbol: '=',
     priority: Infinity,
     handler(buffer, index, context) {
+
         if (index === 0) {
-            Utils.raise(SyntaxError, 'no variable name given', buffer[index], context);
+            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
         }
+
         const nameNode = buffer[index - 1];
         if (nameNode.type !== 'word') {
             Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index], context);
         }
+
         const value = evalExpression(buffer, context, index + 1);
         context.store.set((nameNode as WordNode).value, value);
+
         const valueNode = Utils.createValueNode(value, nameNode);
         Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
+
     },
 }];
 /** dts2md break */
