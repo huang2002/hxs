8ee7bc96ab06585a25ce597a6e23c9929cd2e463
hhh <1340545944@qq.com>
2021-07-02 21:25:34 +0800

    add REPL

---
+++ cli/createREPL.js
@@ -0,0 +1,59 @@
+// @ts-check
+const { createInterface } = require('readline');
+const { Utils, evalCode, builtins, createFunctionHandler } = /** @type {import('..')} */(
+    /** @type {unknown} */(require('../dist/hxs.umd.js'))
+);;
+
+exports.createREPL = () => {
+
+    const interface = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const store = new Map(builtins);
+
+    store.set('__repl', Utils.injectHelp(
+        'REPL manager',
+        Utils.createDict({
+            setPrompt: Utils.injectHelp(
+                '__repl.setPrompt(str)',
+                createFunctionHandler(1, 1, (args, referer, context) => {
+                    if (typeof args[0] !== 'string') {
+                        Utils.raise(TypeError, 'expect a string as prompt', referer, context);
+                    }
+                    interface.setPrompt(/** @type {string} */(args[0]));
+                    return null;
+                })
+            ),
+            exit: Utils.injectHelp(
+                '__repl.exit()',
+                createFunctionHandler(0, 0, () => {
+                    process.exit(0);
+                })
+            ),
+        })
+    ));
+
+    console.log('Entering REPL mode. Invoke `__repl.exit` to exit.\n');
+
+    interface.prompt();
+    interface.on('line', input => {
+        try {
+            console.log(
+                Utils.toString(
+                    evalCode(input, {
+                        store,
+                        source: 'repl',
+                    })
+                )
+            );
+            console.log();
+        } catch (error) {
+            console.error('Error: ' + error.message);
+            console.error();
+        }
+        interface.prompt();
+    });
+
+};
--- cli/index.js
+++ cli/index.js
@@ -1,1 +1,87 @@
 #!/usr/bin/env node
+// @ts-check
+const { Program } = require('3h-cli');
+const { createREPL } = require("./createREPL");
+
+const program = new Program('hxs', {
+    title: require('../package.json').description,
+});
+
+const DEFAULT_ENCODING = 'utf8';
+
+program.action({
+    name: 'exec <files...>',
+    help: 'Execute the given script file(s)',
+});
+
+program.action({
+    name: 'repl',
+    help: 'Start REPL mode in command line',
+});
+
+program.option({
+    name: '--encoding',
+    alias: '-e',
+    value: '<value>',
+    help: `The encoding of input file (default: ${DEFAULT_ENCODING})`,
+});
+
+program.option({
+    name: '--help',
+    alias: '-h',
+    help: 'Print help info',
+});
+
+program.parse(process.argv)
+    .then(args => {
+
+        if (
+            args.options.has('--help')
+            || !(args.actions.length + args.options.size + args.rest.length)
+        ) {
+            program.help();
+            return;
+        }
+
+        if (!args.actions.length) {
+            throw new Error('no actions specified');
+        }
+
+
+        switch (args.actions[0]) {
+
+            case 'exec': {
+                const { readFileSync } = require('fs');
+                const { evalCode } = /** @type {import('..')} */(
+                    /** @type {unknown} */(require('../dist/hxs.umd.js'))
+                );;
+                const encoding = /** @type {import('crypto').Encoding} */(
+                    args.getOption('--encoding')[0] || DEFAULT_ENCODING
+                );
+                const files = args.actions;
+                for (let i = 1; i < files.length; i++) {
+                    const filePath = files[i];
+                    evalCode(
+                        readFileSync(filePath, encoding),
+                        {
+                            source: filePath,
+                        }
+                    );
+                }
+                break;
+            }
+
+            case 'repl': {
+                createREPL();
+                break;
+            }
+
+            default: {
+                throw new Error(
+                    `unknown action: "${args.actions[0]}"`
+                );
+            }
+
+        }
+
+    });
