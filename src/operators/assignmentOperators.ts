6b27fde1741862d8236428563ac947759b6c1f24
hhh <1340545944@qq.com>
2021-08-08 12:33:10 +0800

    add `createAdditionalAssignmentOperator`

--- src/operators/assignmentOperators.ts
+++ src/operators/assignmentOperators.ts
@@ -1,7 +1,7 @@
 import { WordNode } from '3h-ast';
 import { Utils } from '../common';
 import { evalExpression } from "../eval/evalExpression";
-import { OperatorDefinition } from './common';
+import { createAdditionalAssignmentOperator, OperatorDefinition } from './common';
 
 export const assignmentOperators: OperatorDefinition[] = [{
     symbol: '=',
@@ -29,259 +29,63 @@ export const assignmentOperators: OperatorDefinition[] = [{
     symbol: '+=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) + (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a + b)
+    ),
 }, {
     symbol: '-=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) - (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a - b)
+    ),
 }, {
     symbol: '*=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) * (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a * b)
+    ),
 }, {
     symbol: '/=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) / (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a / b)
+    ),
 }, {
     symbol: '&=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) & (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a & b)
+    ),
 }, {
     symbol: '^=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) ^ (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a ^ b)
+    ),
 }, {
     symbol: '|=',
     priority: Infinity,
     ltr: false,
-    handler(buffer, index, context) {
-
-        if (index === 0) {
-            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
-        }
-
-        const nameNode = buffer[index - 1];
-        if (nameNode.type !== 'word') {
-            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
-        }
-
-        const name = (nameNode as WordNode).value;
-        if (!context.store.has(name)) {
-            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
-        }
-
-        const a = context.store.get(name);
-        if (typeof a !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index - 1], context);
-        }
-
-        const b = evalExpression(buffer, context, index + 1);
-        if (typeof b !== 'number') {
-            Utils.raise(SyntaxError, 'expect a number', buffer[index + 1], context);
-        }
-
-        const value = (a as number) | (b as number);
-        context.store.set(name, value);
-
-        const valueNode = Utils.createValueNode(value, nameNode);
-        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
-
-    },
+    handler: createAdditionalAssignmentOperator<number, number>(
+        'number',
+        'number',
+        (a, b) => (a | b)
+    ),
 }];
--- src/operators/common.ts
+++ src/operators/common.ts
@@ -1,5 +1,7 @@
+import { WordNode } from '3h-ast';
 import { ContextValue, SyntaxHandler, Utils } from '../common';
 import { evalBufferNode } from "../eval/evalBufferNode";
+import { evalExpression } from '../eval/evalExpression';
 
 export interface OperatorDefinition {
     symbol: string;
@@ -30,5 +32,48 @@ export const createBinaryOperator = <
             buffer[index]
         );
         Utils.replaceBuffer(buffer, index - 1, 3, valueNode);
+    }
+);
+
+export const createAdditionalAssignmentOperator = <
+    T extends ContextValue,
+    U extends ContextValue
+>(
+    typeA: string,
+    typeB: string,
+    handler: (a: T, b: U) => ContextValue,
+): SyntaxHandler => (
+    (buffer, index, context) => {
+
+        if (index === 0) {
+            Utils.raise(SyntaxError, 'expect a variable name preceding', buffer[index], context);
+        }
+
+        const nameNode = buffer[index - 1];
+        if (nameNode.type !== 'word') {
+            Utils.raise(SyntaxError, 'expect a word as variable name', buffer[index - 1], context);
+        }
+
+        const name = (nameNode as WordNode).value;
+        if (!context.store.has(name)) {
+            Utils.raise(SyntaxError, `"${name}" is not defined`, buffer[index - 1], context);
+        }
+
+        const a = context.store.get(name) as T;
+        if (typeof a !== typeA) {
+            Utils.raise(SyntaxError, `expect a ${typeA}`, buffer[index - 1], context);
+        }
+
+        const b = evalExpression(buffer, context, index + 1) as U;
+        if (typeof b !== typeB) {
+            Utils.raise(SyntaxError, `expect a ${typeB}`, buffer[index + 1], context);
+        }
+
+        const value = handler(a, b);
+        context.store.set(name, value);
+
+        const valueNode = Utils.createValueNode(value, nameNode);
+        Utils.replaceBuffer(buffer, index - 1, buffer.length - index + 1, valueNode);
+
     }
 );
