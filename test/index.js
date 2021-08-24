7b8cf059a89e1ea94f277ecbeef1b8d7957e069f
hhh <1340545944@qq.com>
2021-08-24 10:39:59 +0800

    improve test indices

--- test/index.js
+++ test/index.js
@@ -4,18 +4,20 @@ const { test } = require('3h-test');
 test(null, {
 
     variable: require('./variable.js'),
+    operator: require('./operator.js'),
+
     string: require('./builtins/string.js'),
     array: require('./builtins/array.js'),
     function: require('./builtins/function.js'),
     number: require('./builtins/number.js'),
     math: require('./builtins/math.js'),
-    operator: require('./operator.js'),
     dict: require('./builtins/dict.js'),
     if: require('./builtins/if.js'),
     loop: require('./builtins/loop.js'),
     error: require('./builtins/error.js'),
     class: require('./builtins/class.js'),
     builtins: require('./builtins/builtins.js'),
+
     hello_world: require('./cli/hello_world.js'),
 
 }).catch(
