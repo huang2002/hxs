010e520e9fe27f1105c9d95ebc7670b098c4bd93
hhh <1340545944@qq.com>
2022-07-18 12:48:19 +0800

    improve test code

--- test/magicMethods.js
+++ test/magicMethods.js
@@ -8,41 +8,35 @@ module.exports = (ctx) => {
 
     ctx.assertStrictEqual(
         evalCode(`
-            @assert(a, b) {
-                if (a !== b) {
-                    raise(String('assertion failed: ', a, ' !== ', b));
-                };
-            };
-
             V = Class({
                 @__init(value) {
                     this._v = value;
                 },
                 #__plus -> (value) => (this._v + value),
-                @__minus(value, reverse) {
-                    if (reverse) {
+                @__minus(value, right) {
+                    if (right) {
                         return(value - this._v);
                     } (true) {
                         return(this._v - value);
                     };
                 },
                 #__multiply -> (value) => (this._v * value),
-                @__divide(value, reverse) {
-                    if (reverse) {
+                @__divide(value, right) {
+                    if (right) {
                         return(value / this._v);
                     } (true) {
                         return(this._v / value);
                     };
                 },
-                @__mod(value, reverse) {
-                    if (reverse) {
+                @__mod(value, right) {
+                    if (right) {
                         return(value % this._v);
                     } (true) {
                         return(this._v % value);
                     };
                 },
-                @__pow(value, reverse) {
-                    if (reverse) {
+                @__pow(value, right) {
+                    if (right) {
                         return(value ** this._v);
                     } (true) {
                         return(this._v ** value);
@@ -53,22 +47,22 @@ module.exports = (ctx) => {
                 #__bitOr -> (value) => (this._v | value),
                 #__bitXor -> (value) => (this._v ^ value),
                 #__bitNot -> () => (~this._v),
-                @__leftShift(value, reverse) {
-                    if (reverse) {
+                @__leftShift(value, right) {
+                    if (right) {
                         return(value << this._v);
                     } (true) {
                         return(this._v << value);
                     };
                 },
-                @__rightShift(value, reverse) {
-                    if (reverse) {
+                @__rightShift(value, right) {
+                    if (right) {
                         return(value >> this._v);
                     } (true) {
                         return(this._v >> value);
                     };
                 },
-                @__unsignedRightShift(value, reverse) {
-                    if (reverse) {
+                @__unsignedRightShift(value, right) {
+                    if (right) {
                         return(value >>> this._v);
                     } (true) {
                         return(this._v >>> value);
@@ -77,36 +71,36 @@ module.exports = (ctx) => {
                 #__and -> (value) => (this._v && value),
                 #__or -> (value) => (this._v || value),
                 #__not -> () => (!this._v),
-                @__nullOr(value, reverse) {
-                    if (reverse) {
+                @__nullOr(value, right) {
+                    if (right) {
                         return(value ?? this._v);
                     } (true) {
                         return(this._v ?? value);
                     };
                 },
-                @__gt(value, reverse) {
-                    if (reverse) {
+                @__gt(value, right) {
+                    if (right) {
                         return(value > this._v);
                     } (true) {
                         return(this._v > value);
                     };
                 },
-                @__gte(value, reverse) {
-                    if (reverse) {
+                @__gte(value, right) {
+                    if (right) {
                         return(value >= this._v);
                     } (true) {
                         return(this._v >= value);
                     };
                 },
-                @__lt(value, reverse) {
-                    if (reverse) {
+                @__lt(value, right) {
+                    if (right) {
                         return(value < this._v);
                     } (true) {
                         return(this._v < value);
                     };
                 },
-                @__lte(value, reverse) {
-                    if (reverse) {
+                @__lte(value, right) {
+                    if (right) {
                         return(value <= this._v);
                     } (true) {
                         return(this._v <= value);
@@ -116,104 +110,104 @@ module.exports = (ctx) => {
                 #__notEqual -> (value) => (this._v != value),
             });
 
-            assert(V(1) + 1, 2);
-            assert(V(1) + V(1), 2);
-            assert(1 + V(1), 2);
+            assert(V(1) + 1 === 2);
+            assert(V(1) + V(1) === 2);
+            assert(1 + V(1) === 2);
 
-            assert(V(1) - 1, 0);
-            assert(V(1) - V(1), 0);
-            assert(1 - V(1), 0);
+            assert(V(1) - 1 === 0);
+            assert(V(1) - V(1) === 0);
+            assert(1 - V(1) === 0);
 
-            assert(V(2) * 6, 12);
-            assert(V(2) * V(6), 12);
-            assert(2 * V(6), 12);
+            assert(V(2) * 6 === 12);
+            assert(V(2) * V(6) === 12);
+            assert(2 * V(6) === 12);
 
-            assert(V(6) / 2, 3);
-            assert(V(6) / V(2), 3);
-            assert(6 / V(2), 3);
+            assert(V(6) / 2 === 3);
+            assert(V(6) / V(2) === 3);
+            assert(6 / V(2) === 3);
 
-            assert(V(2) % 6, 2);
-            assert(V(2) % V(6), 2);
-            assert(2 % V(6), 2);
+            assert(V(2) % 6 === 2);
+            assert(V(2) % V(6) === 2);
+            assert(2 % V(6) === 2);
 
-            assert(V(2) ** 6, 64);
-            assert(V(2) ** V(6), 64);
-            assert(2 ** V(6), 64);
+            assert(V(2) ** 6 === 64);
+            assert(V(2) ** V(6) === 64);
+            assert(2 ** V(6) === 64);
 
-            assert(-V(2), -2);
+            assert(-V(2) === -2);
 
-            assert(V(2) & 6, 2);
-            assert(V(2) & V(6), 2);
-            assert(2 & V(6), 2);
+            assert((V(2) & 6) === 2);
+            assert((V(2) & V(6)) === 2);
+            assert((2 & V(6)) === 2);
 
-            assert(V(2) | 6, 6);
-            assert(V(2) | V(6), 6);
-            assert(2 | V(6), 6);
+            assert((V(2) | 6) === 6);
+            assert((V(2) | V(6)) === 6);
+            assert((2 | V(6)) === 6);
 
-            assert(V(2) ^ 6, 4);
-            assert(V(2) ^ V(6), 4);
-            assert(2 ^ V(6), 4);
+            assert((V(2) ^ 6) === 4);
+            assert((V(2) ^ V(6)) === 4);
+            assert((2 ^ V(6)) === 4);
 
-            assert(~V(6), ~6);
+            assert(~V(6) === ~6);
 
-            assert(V(2) << 1, 4);
-            assert(V(2) << V(1), 4);
-            assert(2 << V(1), 4);
+            assert(V(2) << 1 === 4);
+            assert(V(2) << V(1) === 4);
+            assert(2 << V(1) === 4);
 
-            assert(V(2) >> 1, 1);
-            assert(V(2) >> V(1), 1);
-            assert(2 >> V(1), 1);
+            assert(V(2) >> 1 === 1);
+            assert(V(2) >> V(1) === 1);
+            assert(2 >> V(1) === 1);
 
-            assert(V(-2) >>> 1, -2 >>> 1);
-            assert(V(-2) >>> V(1), -2 >>> 1);
-            assert(-2 >>> V(1), -2 >>> 1);
+            assert(V(-2) >>> 1 === -2 >>> 1);
+            assert(V(-2) >>> V(1) === -2 >>> 1);
+            assert(-2 >>> V(1) === -2 >>> 1);
 
-            assert(V(true) && true, true);
-            assert(V(true) && V(false), false);
-            assert(false && V(true), false);
+            assert((V(true) && true) === true);
+            assert((V(true) && V(false)) === false);
+            assert((false && V(true)) === false);
 
-            assert(V(false) || false, false);
-            assert(V(true) || V(false), true);
-            assert(false || V(true), true);
+            assert((V(false) || false) === false);
+            assert((V(true) || V(false)) === true);
+            assert((false || V(true)) === true);
 
-            assert(!V(true), false);
-            assert(!V(false), true);
+            assert(!V(true) === false);
+            assert(!V(false) === true);
 
-            assert(V(null) ?? false, false);
-            assert(V(false) ?? V(true), false);
-            assert(false ?? V(true), false);
+            assert((V(null) ?? false) === false);
+            assert((V(false) ?? V(true)) === false);
+            assert((false ?? V(true)) === false);
 
-            assert(V(2) > 6, false);
-            assert(V(2) > V(6), false);
-            assert(2 > V(6), false);
+            assert((V(2) > 6) === false);
+            assert((V(2) > V(6)) === false);
+            assert((2 > V(6)) === false);
 
-            assert(V(2) < 6, true);
-            assert(V(2) < V(6), true);
-            assert(2 < V(6), true);
+            assert((V(2) < 6) === true);
+            assert((V(2) < V(6)) === true);
+            assert((2 < V(6)) === true);
 
-            assert(V(2) >= 6, false);
-            assert(V(2) >= V(6), false);
-            assert(2 >= V(2), true);
+            assert((V(2) >= 6) === false);
+            assert((V(2) >= V(6)) === false);
+            assert((2 >= V(2)) === true);
 
-            assert(V(2) <= 6, true);
-            assert(V(2) <= V(6), true);
-            assert(2 <= V(2), true);
+            assert((V(2) <= 6) === true);
+            assert((V(2) <= V(6)) === true);
+            assert((2 <= V(2)) === true);
 
-            assert(V(2) == 6, false);
-            assert(V(2) == V(2), true);
-            assert(2 == V(6), false);
+            assert((V(2) == 6) === false);
+            assert((V(2) == V(2)) === true);
+            assert((2 == V(6)) === false);
 
-            assert(V(2) != 6, true);
-            assert(V(2) != V(2), false);
-            assert(2 != V(6), true);
+            assert((V(2) != 6) === true);
+            assert((V(2) != V(2)) === false);
+            assert((2 != V(6)) === true);
 
-            assert(V(2) === 6, false);
-            assert(V(2) === V(2), false);
-            assert(2 === V(6), false);
+            assert((V(2) === 6) === false);
+            assert((V(2) === V(2)) === false);
+            assert((2 === V(6)) === false);
 
-            assert(V(2) !== 6, true);
-            assert(V(2) !== V(2), true);
-            assert(2 !== V(6), true);
+            assert((V(2) !== 6) === true);
+            assert((V(2) !== V(2)) === true);
+            assert((2 !== V(6)) === true);
 
             Map = Class({
                 @__init() {
@@ -253,30 +247,30 @@ module.exports = (ctx) => {
             map = Map();
             o = {};
 
-            assert(typeOf(map.keys), 'array');
+            assert(typeOf(map.keys) === 'array');
 
             map.a = 0;
             map[#b] = 1;
             map[o] = 6;
 
-            assert(map.a, 0);
-            assert(map[#a], null);
-            assert(map.b, null);
-            assert(map[#b], 1);
-            assert(map.c, null);
-            assert(map[#c], null);
-            assert(map.o, null);
-            assert(map[o], 6);
-            assert(map[{}], null);
+            assert(map.a === 0);
+            assert(map[#a] === null);
+            assert(map.b === null);
+            assert(map[#b] === 1);
+            assert(map.c === null);
+            assert(map[#c] === null);
+            assert(map.o === null);
+            assert(map[o] === 6);
+            assert(map[{}] === null);
 
-            assert(map:has(#a), true);
-            assert(map:has(#c), false);
-            assert(map:keys():sizeOf(), 9);
-            assert(keys(map):sizeOf(), 2);
+            assert(map:has(#a) === true);
+            assert(map:has(#c) === false);
+            assert(map:keys():sizeOf() === 9);
+            assert(keys(map):sizeOf() === 2);
 
             map[#a] = 2;
-            assert(map.a, 0);
-            assert(map[#a], 2);
+            assert(map.a === 0);
+            assert(map[#a] === 2);
 
             a = {
                 @__set(key, value) {
@@ -289,10 +283,10 @@ module.exports = (ctx) => {
             b[#foo] = 'bar';
             b[a] = b;
             a:assign(b);
-            assert(a.foo, 'bar');
+            assert(a.foo === 'bar');
 
             b:unpack([#foo]);
-            assert(foo, 'bar');
+            assert(foo === 'bar');
 
             666
         `),
